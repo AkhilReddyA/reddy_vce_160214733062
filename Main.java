a86a2e6b17da9466b30860f8e30be09f1d7edfde
AkhilReddyA <aareddy50309@gmail.com>
2017-09-12 17:43:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Date myDate = new Date(2014, 02, 11);
+		Student st=new Student(1,"Ak",myDate,90.5);
+		StudentGroup s=new StudentGroup(1);
+		s.setStudent(st,0);
+		
+	}
+
+}
